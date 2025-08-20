ea5dc31c1c98c61bcd6f63a2626a82fbffb315a9
Ankit saini <ankitsaini24082002@gmail.com>
2025-08-20 23:10:03 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'v1.0.0';
+const VERSION = 'ei-v1';
 const CORE = [
   './',
   './index.html',
@@ -15,24 +15,21 @@ const CORE = [
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(VERSION).then(cache => cache.addAll(CORE)));
+  e.waitUntil(caches.open(VERSION).then(c => c.addAll(CORE)));
 });
-
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== VERSION && caches.delete(k))))
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== VERSION && caches.delete(k)))));
 });
-
 self.addEventListener('fetch', (e) => {
   const url = new URL(e.request.url);
   if (url.origin === location.origin) {
     e.respondWith(
       caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
         const copy = res.clone();
-        caches.open(VERSION).then(cache => cache.put(e.request, copy));
+        caches.open(VERSION).then(c => c.put(e.request, copy));
         return res;
       }).catch(() => cached))
     );
   }
 });
+
